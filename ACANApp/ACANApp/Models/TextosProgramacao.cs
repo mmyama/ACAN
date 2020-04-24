25973d0968d7e1ed61af2b6eb2b0a1d6226b3f02
Marcelo <mmyama@gmail.com>
2020-04-24 13:32:31 -0300

    Template para caledário

---
+++ ACANApp/ACANApp/Models/TextosProgramacao.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACANApp.Models
+{
+    public class TextosProgramacao
+    {
+        private string janeiro;
+        private string fevereiro;
+        private string marco;
+        private string abril;
+        private string maio;
+        private string junho;
+        private string julho;
+        private string agosto;
+        private string setembro;
+        private string outubro;
+        private string novembro;
+        private string dezembro;
+
+        public string Janeiro 
+        {
+            get
+            {
+                return janeiro;
+            }
+            set
+            {
+                janeiro = value;
+            }
+        }
+        public string Fevereiro
+        {
+            get
+            {
+                return fevereiro;
+            }
+            set
+            {
+                fevereiro = value;
+            }
+        }
+        public string Marco
+        {
+            get
+            {
+                return marco;
+            }
+            set
+            {
+                marco = value;
+            }
+
+        }
+        public string Abril
+        {
+            get
+            {
+                return abril;
+            }
+            set
+            {
+                abril = value;
+            }
+
+        }
+        public string Maio
+        {
+            get
+            {
+                return maio;
+            }
+            set
+            {
+                maio = value;
+            }
+
+        }
+        public string Junho
+        {
+            get
+            {
+                return junho;
+            }
+            set
+            {
+                junho = value;
+            }
+
+        }
+        public string Julho
+        {
+            get
+            {
+                return julho;
+            }
+            set
+            {
+                julho = value;
+            }
+
+        }
+        public string Agosto
+        {
+            get
+            {
+                return agosto;
+            }
+            set
+            {
+                agosto = value;
+            }
+
+        }
+        public string Setembro
+        {
+            get
+            {
+                return setembro;
+            }
+            set
+            {
+                setembro = value;
+            }
+
+        }
+        public string Outubro
+        {
+            get
+            {
+                return outubro;
+            }
+            set
+            {
+                outubro = value;
+            }
+
+        }
+        public string Novembro
+        {
+            get
+            {
+                return novembro;
+            }
+            set
+            {
+                novembro = value;
+            }
+
+        }
+        public string Dezembro
+        {
+            get
+            {
+                return dezembro;
+            }
+            set
+            {
+                dezembro = value;
+            }
+
+        }
+
+        public TextosProgramacao()
+        {
+            Janeiro = "1(SEG) - COVID-19";
+            Fevereiro = "2(TER) - COVID-19";
+            Marco = "3(QUA) - COVID-19";
+            Abril = "4(QUI) - COVID-19";
+            Maio = "5(SEX) - COVID-19";
+            Junho = "6(SAB) - COVID-19";
+            Julho = "7(DOM) - COVID-19";
+            Agosto = "8(SEG) - COVID-19";
+            Setembro = "9(TER) - COVID-19";
+            Outubro = "10(QUA) - COVID-19";
+            Novembro = "11(QUI) - COVID-19";
+            Dezembro = "12(SEX) - COVID-19";
+
+        }
+    }
+}
--- ACANApp/ACANApp/ViewModels/NossoDojoViewModel.cs
+++ ACANApp/ACANApp/ViewModels/NossoDojoViewModel.cs
@@ -16,5 +16,93 @@ namespace ACANApp.ViewModels
                 return TextosNossoDojo.TXTETIQUETA;
             }
         }
+
+        private TextosProgramacao TextosProgramacao = new TextosProgramacao();
+
+        public string TextoJaneiro
+        {
+            get
+            {
+                return TextosProgramacao.Janeiro;
+            }
+        }
+        public string TextoFevereiro
+        {
+            get
+            {
+                return TextosProgramacao.Fevereiro;
+            }
+        }
+        public string TextoMarco
+        {
+            get
+            {
+                return TextosProgramacao.Marco;
+            }
+        }
+        public string TextoAbril
+        {
+            get
+            {
+                return TextosProgramacao.Abril;
+            }
+        }
+        public string TextoMaio
+        {
+            get
+            {
+                return TextosProgramacao.Maio;
+            }
+        }
+        public string TextoJunho
+        {
+            get
+            {
+                return TextosProgramacao.Junho;
+            }
+        }
+        public string TextoJulho
+        {
+            get
+            {
+                return TextosProgramacao.Julho;
+            }
+        }
+        public string TextoAgosto
+        {
+            get
+            {
+                return TextosProgramacao.Agosto;
+            }
+        }
+        public string TextoSetembro
+        {
+            get
+            {
+                return TextosProgramacao.Setembro;
+            }
+        }
+        public string TextoOutubro
+        {
+            get
+            {
+                return TextosProgramacao.Outubro;
+            }
+        }
+        public string TextoNovembro
+        {
+            get
+            {
+                return TextosProgramacao.Novembro;
+            }
+        }
+        public string TextoDezembro
+        {
+            get
+            {
+                return TextosProgramacao.Dezembro;
+            }
+        }
+
     }
 }
